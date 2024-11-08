fa7e84df944148409dce24db53ad2ea886fafd7e
Manu <manu.frenot91@gmail.com>
2024-11-08 01:25:28 +0100

    refactor le package entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './paris/v1/entities/User';
-import { Tournament } from './paris/v1/entities/Tournament';
-import { ChampionshipDay } from './paris/v1/entities/ChampionshipDay';
-import { Match } from './paris/v1/entities/Match';
-import { Bet } from './paris/v1/entities/Bet';
-import { Configuration } from './paris/v1/entities/Configuration';
+import { User } from './entities/paris/v1/User';
+import { Tournament } from './entities/paris/v1/Tournament';
+import { ChampionshipDay } from './entities/paris/v1/ChampionshipDay';
+import { Match } from './entities/paris/v1/Match';
+import { Bet } from './entities/paris/v1/Bet';
+import { Configuration } from './entities/paris/v1/Configuration';
 
 import { UsersModule } from './users/users.module';
 
@@ -21,7 +21,7 @@ import { UsersModule } from './users/users.module';
       database: 'base2',
       entities: ['src/../**/*.entity.js'],  
       database: 'sports_betting',
-	  entities: [__dirname + '/paris/v1/entities/*.{ts,js}'],
+	  entities: [__dirname + '/entities/paris/v1/*.{ts,js}'],
       synchronize: true, // Désactiver en production !
     }),
     TypeOrmModule.forFeature([
